fe6f836f535490dc11be18583a3a8222368651ae
fedykvitalik2004 <vitalii.fedyk.it.2021@lpnu.ua>
2026-03-08 18:49:29 +0200

    refactor: add get all authors

--- code/domain/src/main/java/org/vitalii/fedyk/author/usecase/GetAllAuthorsByLanguageUseCase.java
+++ code/domain/src/main/java/org/vitalii/fedyk/author/usecase/GetAllAuthorsByLanguageUseCase.java
@@ -1,11 +1,8 @@
 package org.vitalii.fedyk.author.usecase;
 
-import org.springframework.core.SpringVersion;
+import java.util.List;
 import org.vitalii.fedyk.author.Author;
 
-import java.util.Optional;
-import java.util.UUID;
-
-public interface GetAuthorUseCase {
-  Author getAuthor(String language, UUID id);
+public interface GetAllAuthorsByLanguageUseCase {
+  List<Author> getAllAuthors(String language);
 }
