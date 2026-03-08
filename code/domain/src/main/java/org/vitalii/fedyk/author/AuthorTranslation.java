3d7215af79d8b4fe615aee783b8c2c59fb5464c4
fedykvitalik2004 <vitalii.fedyk.it.2021@lpnu.ua>
2026-03-08 18:37:14 +0200

    refactor: add author translation model

--- code/domain/src/main/java/org/vitalii/fedyk/author/AuthorTranslation.java
+++ code/domain/src/main/java/org/vitalii/fedyk/author/AuthorTranslation.java
@@ -1,5 +1,29 @@
-package org.vitalii.fedyk.common.author;
+package org.vitalii.fedyk.author;
 
+import java.time.OffsetDateTime;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.vitalii.fedyk.common.enumeration.Language;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AuthorTranslation {
-  private String 
+  private UUID id;
+
+  private OffsetDateTime createdAt;
+
+  private String firstName;
+
+  private String lastName;
+
+  private String description;
+
+  private Language language;
 }
